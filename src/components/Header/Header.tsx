405d6282954f3b0d02d93e0935e0de9cd56dbdd5
Naing Lu Tun <nainglu7924@gmail.com>
2024-10-30 22:19:34 +0630

    changed color theme and added profile image

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div className="bg-transparent headerContainer">
+        <a href="#intro" className="text-[40px] text-[#ccf281]  mainLogo">Naing</a>
+
+    </div>
+  )
+}
+
+export default Header
--- src/components/Main Components/Intro.tsx
+++ src/components/Main Components/Intro.tsx
@@ -1,18 +1,25 @@
 
 
 import "../../index.css"
+import profileImg from "../../assets/profile.png"
+
+import Header from "../Header/Header"
 
 const Intro = () => {
   return (
     <section id="intro" className=" flex w-full introSection">
 
-        <div className="w-[60%] p-[50px] bg-[#3d3149] introTextsContainer">
-
-          <a href="#intro" className="text-[40px] text-[#fd3654]  mainLogo">Naing</a>
+        <div className="w-[60%] p-[100px] bg-[#151730] introTextsContainer">
+          <Header />
+          
         </div>
 
-        <div className="w-[40%] p-[50px] bg-[#fd3654] introImageContainer">
+        <div className="w-[40%] p-[50px] bg-[#ccf281] relative introImageContainer">
 
+            <div className="w-[150px] h-[150px] bg-[#379958] rounded-3xl overflow-hidden profileImgContainer">
+              <img src={profileImg} alt="profile" className="w-full profileImg" />
+            </div>
+            
         </div>
         
 
